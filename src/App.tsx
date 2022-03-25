4ffd739dac6f5bacb5e9b6b7c9d4a28063abeb9f
overthestream <njhnjh02@naver.com>
2022-03-25 17:14:08 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyle from './GlobalStyle';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
