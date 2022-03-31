372f4b0e5ae01e8921abe1b1b99287d8f83f8fa3
overthestream <njhnjh02@naver.com>
2022-04-01 02:05:02 +0900

    feat(markdown): add Markdown Renderer

--- src/components/base/Markdown/MarkdownRenderer.tsx
+++ src/components/base/Markdown/MarkdownRenderer.tsx
@@ -6,6 +6,7 @@ import ResponsiveBox from 'components/base/Responsive';
 import { SpecialComponents } from 'react-markdown/lib/ast-to-react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 import style from 'components/base/Markdown/style';
 
@@ -13,8 +14,65 @@ const MarkdownWrapper = styled(ResponsiveBox)`
   line-height: 2.5rem;
 `;
 
-// 구현할 것 :  <hr/>, h2?쯤 되는 태그에 구분선 넣기. tag 기능. 왼쪽 h별 목차, 목록(ul, li) 좀 더 예쁘게.
-// 이미지 넣는 방식 고안하기
+const HorizontalLine = styled.div`
+  width: 100%;
+  height: 2px;
+  margin-bottom: 5px;
+  background-color: ${oc.gray[3]};
+`;
+
+const HorizontalRule: React.FC = () => {
+  return <HorizontalLine />;
+};
+
+const H1Wrapper = styled.div`
+  font-size: 36px;
+  font-weight: bold;
+  padding: 20px 0px;
+`;
+const H1divider = styled.div`
+  width: 100%;
+  margin-top: 4px;
+  height: 3px;
+  background-color: ${oc.gray[4]};
+`;
+
+const HeaderFirst: React.FC = (props) => {
+  const { children } = props;
+  return (
+    <H1Wrapper>
+      {children}
+      <H1divider />
+    </H1Wrapper>
+  );
+};
+
+const H2Wrapper = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const HeaderSecond: React.FC = (props) => {
+  const { children } = props;
+  return (
+    <H2Wrapper>
+      {children}
+      <HorizontalLine />
+    </H2Wrapper>
+  );
+};
+
+const List: SpecialComponents['li'] = (props) => {
+  const { ordered, children } = props;
+  if (ordered) return <li {...props}>{children}</li>;
+  return (
+    <div>
+      <span>- </span>
+      {children}
+    </div>
+  );
+};
+
 const BlockQuoteWrapper = styled.blockquote`
   width: 100%;
   padding: 0 20px;
@@ -35,7 +93,6 @@ const Code: SpecialComponents['code'] = (props) => {
   const inlineCodeBlock = (
     <SyntaxHighlighter
       PreTag="span"
-      useInlineStyles
       customStyle={{
         padding: '2px 3px 3px 2px',
         margin: 0,
@@ -87,114 +144,30 @@ const Code: SpecialComponents['code'] = (props) => {
 };
 
 const example = `
-<hr/>
-1. hi
 
-2. hi
-
-3. hi
-
-> ghihihihi
-> > nested quote
-
-# React implementation using Typescript
-
-[타입스크립트로 컴포넌트 children 사용하기](https://www.carlrippon.com/react-children-with-typescript/)
-
-## UseState 사용하기
-![image](./hi.png)
-_hi_
-\`cosnt\`
-~~~ts
-const [count, setCount] = useState<number>(0);
-
-// 12312312
-~~~
-
-## Map
-
-배열. Mapping
-
-~~~ts
-arr.map(callback, [thsArg]);
-~~~
-
-- callback : 새로운 배열의 요소를 생성하는 함수.
-  - currentValue: 현재 처리하는 요소
-  - index: 현재 처리하는 요소의 index 값
-  - array: 현재 처리하는 원본 배열
-- thisArg(선택 항목): callback함수에서 사용할 this 레퍼런스
-
-##### 예시
-
-~~~ts
-var numbers: <number> = [1, 2, 3, 4, 5];
-
-var processed = numbers.map(function (num) {
-	return num * num;
-});
-~~~
-
-#### 데이터 배열을 컴포넌트 배열로 변환하기
-
-~~~js
-import React from 'react';
-
-const IterationSample = () => {
-	const names = ['눈사람', '얼음', '눈'];
-	const nameList = names.map((name) => <li>{name}</name>);
-	return <ul>{nameList}</ul>;
-};
-
-export default IterationSample;
-~~~
-
-### 스타일드 컴포넌트
-
-~~~ts
-const Container = styled.div<{ age: number }>;
-~~~
-
-<pre><code>inlinecode</code></pre>
-
-
-# Hello World! 👋
-
-## I'm JeongHoon Rho ([overthestream]) 👨🏻‍💻
-    
-- Junior @ Korea University Dept. of Computer Science & Engineering | 고려대학교 컴퓨터학과 20학번, 3학년
-
-- KU ALPS (Algorithm Learning & Problem Solving) Vice President | 고려대학교 알고리즘 학회 부회장 (2021)
-
-## I'm currently working on ⌨️
-
-    - hi
-    - hi
-    as
-    as
-    as
-    as
-
-## Misc 💻
-
-[![Anurag's github stats](https://github-readme-stats.vercel.app/api?username=overthestream)](https://github.com/anuraghazra/github-readme-stats)
-
-![hyp3rflow's solved.ac stats](https://github-readme-solvedac.hyp3rflow.vercel.app/api/?handle=bln01)
-
-[overthestream]: https://github.com/overthestream
 `;
 
-const MarkdownRenderer: React.FC = () => {
+export interface MarkdownRendererProps {
+  markdown: string;
+}
+
+const MarkdownRenderer: React.FC<MarkdownRendererProps> = (props) => {
+  const { markdown } = props;
   return (
     <MarkdownWrapper>
       <ReactMarkdown
         components={{
           blockquote: BlockQuote,
           code: Code,
+          hr: HorizontalRule,
+          h1: HeaderFirst,
+          h2: HeaderSecond,
+          li: List,
         }}
         remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
       >
-        {example}
+        {markdown}
       </ReactMarkdown>
     </MarkdownWrapper>
   );
